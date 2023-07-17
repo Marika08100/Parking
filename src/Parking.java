36f0d8d16c8699ded3d58ea318485fbe9b63fe36
Marika08100 <132154696+Marika08100@users.noreply.github.com>
2023-07-17 19:08:12 +0300

    is rdy

--- src/Parking.java
+++ src/Parking.java
@@ -5,28 +5,35 @@ public class Parking {
     private final List<Vehicle> vehicles;
     private int capacity;
 
-    public Parking(int capacity){
+    public Parking(int capacity) {
         this.capacity = capacity;
         this.vehicles = new ArrayList<>();
 
     }
-    public boolean isFull(int capacity){
+
+    public boolean isFull(int capacity) {
         return vehicles.size() >= capacity;
     }
-    public void addVehicle(Vehicle vehicle){
-        if(isFull(capacity)){
-            System.out.println("Sorry, the parking house is full! Try again later!");
-        }else {
+
+    public void addVehicle(Vehicle vehicle) {
+        try {
+            if (isFull(capacity)) {
+                throw new IsFullException("The parking lot is full!");
+            }
             vehicles.add(vehicle);
-            System.out.println( "The vehicle wit color " + vehicle.getColor() + " is in the parking lot!");
+            System.out.println("The vehicle with color " + vehicle.getColor() + " is in the parking lot!");
+        } catch (IsFullException e) {
+            System.out.println(e.getMessage());
         }
     }
-    public void removeVehicle(Vehicle vehicle){
+
+
+    public void removeVehicle(Vehicle vehicle) {
         if (vehicles.contains(vehicle)) {
             vehicles.remove(vehicle);
-            System.out.println("The vehicle with color " + vehicle.getColor() + " is not in the parking lot!");
-        }else{
-            System.out.println("The vehicle is out of the parking house!");
+            System.out.println("The vehicle with color " + vehicle.getColor() + " is out of the parking lot!");
+        } else {
+            System.out.println("The vehicle is not in the parking house!");
         }
 
     }
